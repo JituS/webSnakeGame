e033c750d0a3be4c39f1b8e397fa23137d81ef77
jitendra sachdeva <jitendrs@INjitendrs.local>
2016-02-22 14:52:27 +0530

    public file moved to public folder

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var express = require('express');
 var http = require('http');
 var app = express();
 
-app.use(express.static('./'));
+app.use(express.static('public/.'));
 app.get('/', function(req, res){
+	console.log('--')
 	res.redirect('snake.html');
 })
 var server = http.createServer(app).listen(process.env.OPENSHIFT_NODEJS_PORT || 8080,process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
