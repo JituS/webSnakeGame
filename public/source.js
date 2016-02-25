6238f545d972e1e330116dfd548745a5d0abef7b
jitendra sachdeva <jitendrs@thoughtworks.com>
2016-02-25 23:15:37 +0530

    done

--- public/source.js
+++ public/source.js
@@ -1,56 +1,125 @@
 var interval = null;
 
-var snake = {
-	body: null,
-	direction: null,
-	score: 0
-};
-
-var food = {
-	x:100,
-	y:30
+var Snake = function(){
+	this.body = null;
+	this.direction = null;
+	this.food = {};
 }
 
-var init = function(){
-	snake.body = [
+Snake.prototype.init = function(){
+	this.body = [
 		{x:30, y:390, background:'black'},
 		{x:30, y:380, background:'black'},
 		{x:30, y:370, background:'red'},
 	];
+	this.produceFood();
+}
+
+Snake.prototype.produceFood = function(){
+	var req = new XMLHttpRequest()
+	var self = this;
+	req.onreadystatechange = function(){
+	    if (req.readyState == 4 && req.status == 200){
+	    	var food = JSON.parse(req.responseText);
+			var element = document.querySelector(".food");
+			element.style.marginLeft = food.x + "px";
+			element.style.marginTop = food.y + "px";
+			document.querySelector(".food").style.background = "black";
+			self.food = food;
+		}
+	}
+	req.open('get', 'food', true);
+	req.send();
 };
 
+
+Snake.prototype.eatFood = function(){
+	if(this.body[0].x == this.food.x && this.body[0].y == this.food.y){
+		document.querySelector(".food").style.background = "#d8d8d8";
+		var soundTag = document.querySelector('#eat');
+		soundTag.play();
+		var score = document.querySelector(".score");
+		var currentScore = score.textContent.split(":")[1];
+		document.querySelector(".score").textContent = "Score:" + (++currentScore);
+		scoreRequest(currentScore, this.food);
+		this.body.push({x:this.body[0].x, y:this.body[0].y});
+		this.produceFood();
+	}
+}
+
 var tasks = {
-	increment:function(cordinate){
+	increment:function(cordinate, snake){
 		snake.body[0][cordinate]+=10;
 	},
-	decrement:function(cordinate){
+	decrement:function(cordinate, snake){
 		snake.body[0][cordinate]-=10;
 	}
 }
 
-var move = function(direction, task, taskOn){
+Snake.prototype.move = function(direction, task, taskOn){
 	state.curruntStatus = "resume";
-	eatFood();
-	var preX = snake.body[0].x;
-	var preY = snake.body[0].y;
-	task(taskOn);
-	printSnake(preX, preY);
-	snake.direction = direction;
-	isDead();
+	this.eatFood();
+	var preX = this.body[0].x;
+	var preY = this.body[0].y;
+	task(taskOn, this);
+	this.print(preX, preY);
+	this.direction = direction;
+	this.isDead();
+}
+
+
+Snake.prototype.isDead = function(){
+	if(this.body[0].x == -10
+		||this.body[0].y == -10
+		||this.body[0].x == 500
+		||this.body[0].y == 500){
+		this.remove();
+		resetGame();
+	}
+	for(var i = 1; i < this.body.length; i++){
+		if(this.body[0].x == this.body[i].x && this.body[0].y == this.body[i].y){
+			this.remove();
+			resetGame();
+		}
+	}	
+}
+
+Snake.prototype.createSnakeBody = function(){
+	for(var  i = 0; i < this.body.length; i++){
+		var element = document.createElement("div");
+		element.style.background = this.body[i].background;
+		element.setAttribute("class", "snake");
+		element.style.marginLeft = this.body[i].x + "px"; 
+		element.style.marginTop = this.body[i].y + "px"; 
+		document.querySelector("#playArea").appendChild(element);
+	}
+}
+
+Snake.prototype.print = function(preX, preY){
+	this.remove();
+	for(var i = 1; i < this.body.length;i++){
+		var _preX = this.body[i].x;
+		var _preY = this.body[i].y;
+		this.body[i].y = preY;
+		this.body[i].x = preX;
+		preX = _preX;
+		preY = _preY;
+	}
+	this.createSnakeBody();
 }
 
 var directions = {
-	up:function(){
-		move("up", tasks.decrement, 'y');
+	up:function(snake){
+		snake.move("up", tasks.decrement, 'y');
 	},
-	down:function(){
-		move("down", tasks.increment, 'y');
+	down:function(snake){
+		snake.move("down", tasks.increment, 'y');
 	},
-	left:function(){
-		move("left", tasks.decrement, 'x');
+	left:function(snake){
+		snake.move("left", tasks.decrement, 'x');
 	},
-	right:function(){
-		move("right", tasks.increment, 'x');
+	right:function(snake){
+		snake.move("right", tasks.increment, 'x');
 	}
 };
 
@@ -71,85 +140,51 @@ var state = {
 	curruntStatus:"resume"
 }
 
-var createSnake = function(){
-	for(var  i = 0; i < snake.body.length; i++){
-		var element = document.createElement("div");
-		element.style.background = snake.body[i].background;
-		element.setAttribute("class", "snake");
-		element.style.marginLeft = snake.body[i].x + "px"; 
-		element.style.marginTop = snake.body[i].y + "px"; 
-		document.querySelector("#playArea").appendChild(element);
-	}
-}
 
-var printSnake = function(preX, preY){
-	removeSnake();
-	for(var i = 1; i < snake.body.length;i++){
-		var _preX = snake.body[i].x;
-		var _preY = snake.body[i].y;
-		snake.body[i].y = preY;
-		snake.body[i].x = preX;
-		preX = _preX;
-		preY = _preY;
-	}
-	createSnake();
-}
 
-var scoreRequest = function(score){
+var scoreRequest = function(score, preFood){
 	var req = new XMLHttpRequest()
 	req.onreadystatechange = function(){
 	    if (req.readyState == 4 && req.status == 200) {
-	    	console.log(req.responseText)
 	    	document.querySelector('#highscore').textContent = "Highscore:"+(+req.responseText);
 		}
 	}
-	req.open('post', 'highscore', false);
-	req.send('highScore='+score+'&snake='+JSON.stringify(snake));
+	req.open('post', 'highscore', true);
+	req.send('highScore='+score+'&preFood='+JSON.stringify(preFood));
 }
 
-var eatFood = function(){
-	if(snake.body[0].x == food.x && snake.body[0].y == food.y){
-		var soundTag = document.querySelector('#eat');
-		soundTag.play();
-		var score = document.querySelector(".score");
-		var currentScore = ++snake.score;
-		document.querySelector(".score").textContent = "Score:" + (currentScore);
-		scoreRequest(currentScore);
-		snake.body.push({x:snake.body[0].x, y:snake.body[0].y});
-		produceFood();
-	}
-};
-
-var produceFood = function(){
-	food.x = Math.floor(Math.random()*40) * 10;
-	food.y = Math.floor(Math.random()*40) * 10;
-	var element = document.querySelector(".food");
-	element.style.marginLeft = food.x + "px";
-	element.style.marginTop = food.y + "px";
-};
-
-var removeSnake = function(){
+Snake.prototype.remove = function(){
 	var element = document.querySelectorAll(".snake");
 	for(var  i = 0; i < element.length; i++){
 		element[i].parentNode.removeChild(element[i]);
 	}
+
 }
 
 var reset = function(){
-	snake.body = [
-		{x:30, y:390},
-		{x:30, y:380},
-		{x:30, y:370},
-	];
-	removeSnake();
-	init();
-	createSnake();
+	var snake = new Snake();
+	snake.init();
+	snake.createSnakeBody();
 	snake.direction = null;
-	snake.score = 0;
 	snake.body.reverse();
+	document.onkeydown = function(e){
+		checkKey(e, snake);
+	}
+}
+
+var show = function(){
+	var notification = document.querySelector('#information');
+	notification.textContent = 'Your snake is dead';
+	document.querySelector('.main').setAttribute("class","main hidden");
+	document.querySelector('.header').setAttribute("class","header hidden");
+	var middleHeader = document.querySelector('#middleHeader')
+	middleHeader.setAttribute('class','show');		
+	var topHeader = document.querySelector('#topHeader')
+	topHeader.setAttribute('class','show');			
 }
 
 var resetGame = function(){
+	show();
 	clearInterval(interval);
 	var crash = document.querySelector('#crash');
 	crash.play();
@@ -157,19 +192,6 @@ var resetGame = function(){
 	reset();
 }
 
-var isDead = function(){
-	if(snake.body[0].x == -10
-		||snake.body[0].y == -10
-		||snake.body[0].x == 500
-		||snake.body[0].y == 500){
-		resetGame();
-	}
-	for(var i = 1; i < snake.body.length; i++){
-		if(snake.body[0].x == snake.body[i].x && snake.body[0].y == snake.body[i].y){
-			resetGame();
-		}
-	}
-}
 
 var createPlayArea = function(){
 	var playArea = document.querySelector("#playArea");
@@ -190,34 +212,38 @@ var createPlayArea = function(){
 
 var keyCodes = {'38':{canGo:'up',cantGo:'down'},'39':{canGo:'right',cantGo:'left'},'40':{canGo:'down',cantGo:'up'},'37':{canGo:'left',cantGo:'right'}};
 
-var chooseDirection = function(direction){
+var chooseDirection = function(direction, snake){
 	if(snake.direction !== direction.cantGo){
 		clearInterval(interval);
-	    interval = setInterval(directions[direction.canGo], 55);
+	    interval = setInterval(function(){
+	    	directions[direction.canGo](snake);
+	    }, 55);
 	}
 }
 
-function checkKey(e) {
+function checkKey(e, snake) {
     e = e || window.event;
-    chooseDirection(keyCodes[e.keyCode]);
+    chooseDirection(keyCodes[e.keyCode], snake);
 }
 
 var main = function(){
 	document.querySelector('a').onclick = function(){
-		document.querySelector('.main').setAttribute("class","main");
-		document.querySelector('.header').setAttribute("class","header");
+		document.querySelector('.main').setAttribute("class","show main");
+		document.querySelector('.header').setAttribute("class","show header");
 		var middleHeader = document.querySelector('#middleHeader')
-		middleHeader.parentNode.removeChild(middleHeader);		
+		middleHeader.setAttribute('class', 'hidden');		
 		var topHeader = document.querySelector('#topHeader')
-		topHeader.parentNode.removeChild(topHeader);
+		topHeader.setAttribute('class','hidden');
 	}
-	init();
 	createPlayArea();
-	createSnake();
-	produceFood();
-	scoreRequest(0);
+	var snake = new Snake();
+	snake.init();
+	snake.createSnakeBody();
+	scoreRequest(0, snake.food);
 	snake.body.reverse();
-	document.onkeydown = checkKey;
+	document.onkeydown = function(e){
+		checkKey(e, snake);
+	}
 	document.querySelector("#pause").onclick = state.pause;
 	document.querySelector("#resume").onclick = state.resume;
 };
--- server.js
+++ server.js
@@ -2,10 +2,24 @@ var express = require('express');
 var http = require('http');
 var app = express();
 var highscore = 0;
+var myFood = {};
 app.use(express.static('public/.'));
+
 app.get('/', function(req, res){
 	res.redirect('snake.html');
 });
+
+app.get('/food', function(req, res){
+	myFood.x = Math.floor(Math.random()*40) * 10;
+	myFood.y = Math.floor(Math.random()*40) * 10;
+	res.json(myFood);
+});
+
+var validate = function(food, score){
+	console.log(score, highscore, myFood, food);
+	console.log(+score > highscore && food.x == myFood.x && food.y == myFood.y);
+	return +score > highscore && food.x == myFood.x && food.y == myFood.y;
+}
 
 app.post('/highscore', function(req, res){
 	var data = '';
@@ -13,14 +27,14 @@ app.post('/highscore', function(req, res){
 		data+=chunk;
 	});
 	req.on('end',function(){
-		var score = data.split('&')[0];
-		var snake = data.split('&')[1];
-		var score = +score.split('=')[1];
-		var snake = JSON.parse(snake.split('=')[1]);
-		if(+score >= highscore && snake.body.length-2 == score){
+		var score = +data.split('&')[0].split('=')[1];
+		var food = JSON.parse(data.split('&')[1].split('=')[1]);
+		if(validate(food, score)){
 			highscore = score;
+			res.end(highscore.toString());
+		}else{
+			res.end(highscore.toString());
 		}
 	});
-	res.end(highscore.toString());
 })
 var server = http.createServer(app).listen(process.env.OPENSHIFT_NODEJS_PORT || 8080,process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
